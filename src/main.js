9bbbab8e3c7bfd2a4abd5490c87e80ebd1aebf56
Akos Kemives <akoskm@gmail.com>
2023-01-22 08:34:03 +0100

    add prettier, format on save

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-const {app, BrowserWindow, Menu, Tray, ipcMain} = require('electron');
-const path = require('path');
-const ioHook = require('iohook');
-const fs = require('fs');
-const PImage = require('pureimage');
+const { app, BrowserWindow, Menu, Tray, ipcMain } = require("electron");
+const path = require("path");
+const ioHook = require("iohook");
+const fs = require("fs");
+const PImage = require("pureimage");
 
 let tray = null;
 let goal = 1000;
@@ -10,25 +10,25 @@ let wordCount = 0;
 const SPACE = 57;
 const ENTER = 28;
 
-const indexHtmlPath = path.resolve( __dirname,'../resources/index.html');
+const indexHtmlPath = path.resolve(__dirname, "../resources/index.html");
 
-function getProgressBarPath () {
-  return path.resolve( __dirname,'../resources/progress_bar.png');
+function getProgressBarPath() {
+  return path.resolve(__dirname, "../resources/progress_bar.png");
 }
-const preloadScriptPath = path.join(__dirname, 'preload.js');
+const preloadScriptPath = path.join(__dirname, "preload.js");
 
-const font = PImage.registerFont(path.resolve( __dirname, '../resources/RobotoMono-VariableFont_wght.ttf'), 'Roboto');
+const font = PImage.registerFont(path.resolve(__dirname, "../resources/RobotoMono-VariableFont_wght.ttf"), "Roboto");
 font.loadSync();
 
 async function createProgressBarImage() {
   const progressInPercent = (wordCount / goal) * 100;
   var img = PImage.make(100, 20);
-  var ctx = img.getContext('2d');
-  ctx.fillStyle = '#ffffff';
+  var ctx = img.getContext("2d");
+  ctx.fillStyle = "#ffffff";
   ctx.fillRect(0, 0, 100, 20);
-  ctx.fillStyle = '#00ff00';
+  ctx.fillStyle = "#00ff00";
   ctx.fillRect(0, 0, wordCount < goal ? progressInPercent : 100, 20);
-  ctx.fillStyle = '#0000ff';
+  ctx.fillStyle = "#0000ff";
   ctx.font = "20pt Roboto";
   ctx.fillText(`${wordCount}/${goal}`, 5, 17);
   await PImage.encodePNGToStream(img, fs.createWriteStream(getProgressBarPath()));
@@ -41,17 +41,20 @@ async function updateProgressBar(tray) {
 
 function reloadTrayTitle(tray) {
   const title = `Daily goal: ${wordCount} / ${goal} words`;
-  tray.setToolTip(title)
+  tray.setToolTip(title);
 }
 
 const createWindow = async () => {
   const win = new BrowserWindow({
-    width: 400, height: 400, webPreferences: {
-      contextIsolation: true, preload: preloadScriptPath,
-    }
-  })
+    width: 400,
+    height: 400,
+    webPreferences: {
+      contextIsolation: true,
+      preload: preloadScriptPath,
+    },
+  });
 
-  win.loadFile(indexHtmlPath)
+  win.loadFile(indexHtmlPath);
 
   tray = new Tray(getProgressBarPath());
   updateProgressBar(tray);
@@ -59,55 +62,65 @@ const createWindow = async () => {
   let isTracking = false;
   let isHidden = false;
 
-  const menuTemplate = [{
-    id: 'tracker', label: getLabelText(), click: () => {
-      isTracking = !isTracking;
-      if (isTracking) ioHook.start();
+  const menuTemplate = [
+    {
+      id: "tracker",
+      label: getLabelText(),
+      click: () => {
+        isTracking = !isTracking;
+        if (isTracking) ioHook.start();
 
-      menuTemplate[0].label = getLabelText();
-      const contextMenu = Menu.buildFromTemplate(menuTemplate)
-      tray.setContextMenu(contextMenu)
-    }
-  }, {
-    id: 'hide', label: 'Hide', click: () => {
-      if (isHidden) {
-        app.show();
-        menuTemplate[1].label = 'Hide';
-      } else {
-        app.hide()
-        menuTemplate[1].label = 'Show';
-      }
-      isHidden = !isHidden;
+        menuTemplate[0].label = getLabelText();
+        const contextMenu = Menu.buildFromTemplate(menuTemplate);
+        tray.setContextMenu(contextMenu);
+      },
+    },
+    {
+      id: "hide",
+      label: "Hide",
+      click: () => {
+        if (isHidden) {
+          app.show();
+          menuTemplate[1].label = "Hide";
+        } else {
+          app.hide();
+          menuTemplate[1].label = "Show";
+        }
+        isHidden = !isHidden;
 
-      const contextMenu = Menu.buildFromTemplate(menuTemplate)
-      tray.setContextMenu(contextMenu)
-    }
-  }, {
-    id: 'quit', label: 'Quit', click: () => {
-      app.exit();
-    }
-  }]
+        const contextMenu = Menu.buildFromTemplate(menuTemplate);
+        tray.setContextMenu(contextMenu);
+      },
+    },
+    {
+      id: "quit",
+      label: "Quit",
+      click: () => {
+        app.exit();
+      },
+    },
+  ];
 
   function getLabelText() {
-    return isTracking ? 'Stop tracking' : 'Start tracking'
+    return isTracking ? "Stop tracking" : "Start tracking";
   }
 
-  const contextMenu = Menu.buildFromTemplate(menuTemplate)
-  tray.setContextMenu(contextMenu)
+  const contextMenu = Menu.buildFromTemplate(menuTemplate);
+  tray.setContextMenu(contextMenu);
   updateProgressBar(tray);
-  reloadTrayTitle(tray)
+  reloadTrayTitle(tray);
 
-  console.log('registering keydown event');
+  console.log("registering keydown event");
 
   let previousEvent;
 
-  ioHook.on('keydown', (event) => {
+  ioHook.on("keydown", (event) => {
     if (!isTracking) return;
 
     if (event.keycode === SPACE || event.keycode === ENTER) {
       if (previousEvent.keycode !== SPACE && previousEvent.keycode !== ENTER) {
-        wordCount += 1
-        reloadTrayTitle(tray)
+        wordCount += 1;
+        reloadTrayTitle(tray);
         updateProgressBar(tray);
       }
     }
@@ -115,14 +128,14 @@ const createWindow = async () => {
     previousEvent = event;
   });
 
-  ipcMain.handle('set-goal', async function setGoal(_event, wordGoal) {
+  ipcMain.handle("set-goal", async function setGoal(_event, wordGoal) {
     goal = wordGoal;
     updateProgressBar(tray);
     reloadTrayTitle(tray);
-  })
-}
+  });
+};
 
 app.whenReady().then(async () => {
   await createProgressBarImage();
   createWindow();
-})
+});
